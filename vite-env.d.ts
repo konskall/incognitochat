0fe944c55d4d906964e8618c18b2a97b475cdeaa
konskall <konskall@gmail.com>
2025-12-09 21:24:09 +0200

    Update and rename vite.config.ts to vite-env.d.ts

---
+++ vite-env.d.ts
@@ -0,0 +1,10 @@
+// Manually define Vite env types to fix "Cannot find type definition file" error
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly [key: string]: string | boolean | undefined;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
