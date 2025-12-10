64a2a299962670560dc6a880ad975caa1e4ad86d
konskall <konskall@gmail.com>
2025-12-10 23:55:58 +0200

    Update supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Hardcoded keys for development environment where .env is not available.
 const supabaseUrl = 'https://qygirixqsuraclbdfnjp.supabase.co';
-const supabaseAnonKey = 'sb_publishable_Lby5hzrKRwckHzC2itzEAA_VZp1A-Tn';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5Z2lyaXhxc3VyYWNsYmRmbmpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyOTA4NjIsImV4cCI6MjA4MDg2Njg2Mn0.x1KpxEUDQ4EOW58MgsgeKJ5Y9NIqcRIgKmZ-qhkhWZQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
